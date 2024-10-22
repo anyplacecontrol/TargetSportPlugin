d225e5d5f7b907266a788f5e8dc2845bb5c1d7dd
anyplacecontrol <40732543+anyplacecontrol@users.noreply.github.com>
2024-10-22 18:10:50 +0300

    apply discount

--- src/redux/modules/membership.js
+++ src/redux/modules/membership.js
@@ -1,5 +1,8 @@
 import * as uiRedux from './ui'
 import * as membershipApi from '../../api/membershipApi'
+import {MEMBERSHIP_DISCOUNT} from '../../const/general'
+import {sendMessageToParent} from '../../shared/utils/sharedFunctions'
+import * as pluginCommands from '../../shared/const/pluginCommands'
 
 export function loginToMembership(login, password) {
   return async function (dispatch) {
@@ -7,8 +10,13 @@ export function loginToMembership(login, password) {
     try {
       const response = await membershipApi.loginToMembership(login, password)
       if (response.success && response.hasDiscount) {
-        dispatch(uiRedux.showAlert('Discount available', 'You have successfully logged in and have a discount'))
-        //todo: leave plugin
+        window.logger.info(`You have successfully logged in and have a discount ${MEMBERSHIP_DISCOUNT}%`)
+
+        sendMessageToParent(pluginCommands.CMD_APPLY_DISCOUNT, {
+          discountValue: MEMBERSHIP_DISCOUNT,
+          messageHeader: 'Time to Start Shopping!',
+          messageText: `Your standard membership gets you a ${MEMBERSHIP_DISCOUNT}% discount!`
+        })
         return
       }
       if (response.success && !response.hasDiscount) {
