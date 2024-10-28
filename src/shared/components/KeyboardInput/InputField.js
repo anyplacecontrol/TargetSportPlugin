834d4b1f4eb19cd39e62eb4619b1b823d91109d6
Mr. Crabs <taras@madency.com>
2024-10-28 16:46:37 +0200

    AS-6787: Plugin - FE(chore): Refactor InputField to enter passwords

--- src/shared/components/KeyboardInput/InputField.js
+++ src/shared/components/KeyboardInput/InputField.js
@@ -1,29 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {strings} from './strings'
+import * as s from '../../../svg'
+import * as u from '../../utils/sharedFunctions'
 
 export const InputField = ({isPassword, onCancelKeyboard, text, setText, inputRef}) => {
-  return (
-    <form className="search__form ">
+  const [isPasswordVisible, setPasswordVisible] = useState(false)
+
+  const fieldType = () => {
+    if (isPassword) {
+      return isPasswordVisible ? `text` : `password`
+    }
+
+    return `text`
+  }
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!isPasswordVisible)
+  }
+
+  const RenderTogglePasswordBtn = () => {
+    if (!isPassword) return null
+
+    return (
       <button
-        className="search__form--cancel "
-        type="button"
+        className={`col-cen-cen bg-transparent fg-white abs-l abs-center-y`}
+        style={{
+          right: `1rem`,
+          width: `46px`,
+          height: `46px`
+        }}
+        type={`button`}
+        onClick={togglePasswordVisibility}
+      >
+        {isPasswordVisible ? (
+          <s.Eye className={`full-sz`} />
+        ) : (
+          <s.EyeSlash className={`full-sz`} />
+        )}
+      </button>
+    )
+  }
+
+  return (
+    <div
+      className={`modal-keyboard`}
+    >
+      <input
+        className={u.createClass(`modal-keyboard-input`, {'--with-eye': isPassword})}
+        type={fieldType()}
+        placeholder={strings.PleaseTypeIn}
+        onInput={(event) => {
+          setText(event.target.value)
+        }}
+        value={text}
+        ref={inputRef}
+        readOnly
+      />
+      <button
+        className={`modal-keyboard-cancel abs-b-r`}
+        type={`button`}
         onClick={onCancelKeyboard}
       />
-      <div className="search__form--inner relative w-full">
-        <input
-          className="search__form--input "
-          type={isPassword ? `password` : `text`}
-          placeholder={strings.PleaseTypeIn}
-          onInput={(event) => {
-            setText(event.target.value)
-          }}
-          value={text}
-          ref={inputRef}
-          readOnly
-        />
-      </div>
-    </form>
+      {RenderTogglePasswordBtn()}
+    </div>
   )
 }
 
